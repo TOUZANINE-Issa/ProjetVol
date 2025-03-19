98ce7573ba34ba90d73a00d99c930ccd5149e0b4
Touzanine Issa <i.touzanine@lprs.fr>
2025-03-19 11:30:45 +0100

    add : ajout du git template et package

---
+++ src/Database/Bdd.php
@@ -0,0 +1,19 @@
+<?php
+class   Bdd {
+    private $bdd;
+
+    public function __construct($host = 'localhost', $dbname = 'projetvol', $username = 'root', $password = '') {
+        try {
+            $this->bdd = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+            $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Erreur de connexion : " . $e->getMessage());
+        }
+    }
+
+    public function getBdd() {
+        return $this->bdd;
+    }
+}
+?>
+
